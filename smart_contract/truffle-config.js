29fb8d3e0d830f9e4827ebe1ab9a3c81805647e0
winniehhy <yoyowinnie3@gmail.com>
2024-09-11 02:01:34 +0800

    successful to deploy

--- smart_contract/truffle-config.js
+++ smart_contract/truffle-config.js
@@ -50,7 +50,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545, // Port Ganache CLI is running on
+      port: 7545, // Port Ganache CLI is running on
       network_id: "*", // Match any network id
     },
   },
