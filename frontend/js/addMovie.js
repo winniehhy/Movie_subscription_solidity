6be31bbeef49edbd555ecb8ba8f3b08162a50d24
Low-JH <lowjianhao43@gmail.com>
2024-09-26 00:18:16 +0800

    Add files via upload

--- frontend/js/addMovie.js
+++ frontend/js/addMovie.js
@@ -1,9 +1,15 @@
+import Web3 from 'web3';
+import { create } from 'ipfs-http-client';
+
 let web3;
 let movieSubscriptionSystem;
 
 import contractData from '../../smart_contract/build/contracts/ContentManagement.json';
 const contractAddress = contractData.networks[5777]?.address;  // smart contract address
 const contractABI = contractData.abi; // smart contract's ABI
+
+// Initialize IPFS client
+const ipfs = create({ url: 'https://ipfs.infura.io:5001/api/v0' });
 
 // MetaMask connection
 window.addEventListener('load', async () => {
@@ -24,17 +30,19 @@ async function submitMovieForm() {
     const releaseDate = Math.floor(new Date(document.getElementById('releaseDate').value).getTime() / 1000);
     const endDate = Math.floor(new Date(document.getElementById('endDate').value).getTime() / 1000);
     const description = document.getElementById('description').value;
-
-    const accounts = localStorage.getItem('userAddress');
-    const account = accounts[0];
+    const imageFile = document.getElementById('image').files[0];
 
     try {
+        // Upload image to IPFS
+        const added = await ipfs.add(imageFile);
+        const imageUrl = `https://ipfs.infura.io/ipfs/${added.path}`; // Get IPFS URL
+
         // Get accounts from MetaMask
         const accounts = await web3.eth.getAccounts();
         const account = accounts[0];  // Use the first account
 
         // Interact with the contract
-        await movieSubscriptionSystem.methods.createMovie(name, rate, releaseDate, endDate, description)
+        await movieSubscriptionSystem.methods.createMovie(name, rate, releaseDate, endDate, description, imageUrl)
             .send({ from: account });
 
         alert('Movie created successfully!');
