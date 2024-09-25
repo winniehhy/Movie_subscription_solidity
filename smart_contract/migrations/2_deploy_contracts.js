6dfe4f25198fe526926fc45e7945cd49043d9500
Low-JH <lowjianhao43@gmail.com>
2024-09-25 21:44:35 +0800

    Update 2_deploy_contracts.js

--- smart_contract/migrations/2_deploy_contracts.js
+++ smart_contract/migrations/2_deploy_contracts.js
@@ -2,10 +2,12 @@ const UserManagement = artifacts.require("UserManagement");
 const TransactionPayment = artifacts.require("TransactionPayment");
 const MovieVoting = artifacts.require("MovieVoting");
 const NotificationManagement = artifacts.require("NotificationManagement");
+const ContentManagement = artifacts.require("ContentManagement");
 
 module.exports = function(deployer) {
     deployer.deploy(UserManagement);
     deployer.deploy(TransactionPayment);
     deployer.deploy(MovieVoting);
     deployer.deploy(NotificationManagement);
+    deployer.deploy(ContentManagement);
 };
