a1a776804a0213cb0f6fdc00f9dfee8fc4f89d80
Low-JH <lowjianhao43@gmail.com>
2024-09-26 00:33:17 +0800

    Add files via upload

--- frontend/js/addMovie.js
+++ frontend/js/addMovie.js
@@ -1,5 +1,4 @@
-import Web3 from 'web3';
-import { create } from 'ipfs-http-client';
+
 
 let web3;
 let movieSubscriptionSystem;
@@ -8,8 +7,6 @@ import contractData from '../../smart_contract/build/contracts/ContentManagement
 const contractAddress = contractData.networks[5777]?.address;  // smart contract address
 const contractABI = contractData.abi; // smart contract's ABI
 
-// Initialize IPFS client
-const ipfs = create({ url: 'https://ipfs.infura.io:5001/api/v0' });
 
 // MetaMask connection
 window.addEventListener('load', async () => {
@@ -30,27 +27,22 @@ async function submitMovieForm() {
     const releaseDate = Math.floor(new Date(document.getElementById('releaseDate').value).getTime() / 1000);
     const endDate = Math.floor(new Date(document.getElementById('endDate').value).getTime() / 1000);
     const description = document.getElementById('description').value;
-    const imageFile = document.getElementById('image').files[0];
 
+    // Get accounts from MetaMask
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];  // Use the first account
+
+    // Interact with the contract
     try {
-        // Upload image to IPFS
-        const added = await ipfs.add(imageFile);
-        const imageUrl = `https://ipfs.infura.io/ipfs/${added.path}`; // Get IPFS URL
-
-        // Get accounts from MetaMask
-        const accounts = await web3.eth.getAccounts();
-        const account = accounts[0];  // Use the first account
-
-        // Interact with the contract
-        await movieSubscriptionSystem.methods.createMovie(name, rate, releaseDate, endDate, description, imageUrl)
+        await movieSubscriptionSystem.methods.createMovie(name, rate, releaseDate, endDate, description)
             .send({ from: account });
 
         alert('Movie created successfully!');
     } catch (error) {
-        console.error('Error creating movie:', error);
+        console.error(error);
         alert('Error creating movie.');
     }
-}
+};
 
 // Event listener for form submission
 document.getElementById('movieForm').addEventListener('submit', async (event) => {
